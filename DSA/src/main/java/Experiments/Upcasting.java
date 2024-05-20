c31f2a704d3527c66b6f6e96b43c1989c8dea089
SatishPunekar <satish.punekar@lytx.com>
2024-05-20 16:17:25 +0530

    committed

---
+++ DSA/src/main/java/Experiments/Upcasting.java
@@ -0,0 +1,10 @@
+package Experiments;
+
+public class Upcasting {
+	public static void main(String[] args) {
+		int a=255;
+		byte b=(byte) a;
+		System.out.println(b);
+	}
+
+}
--- DSA/src/main/java/arrayList/ArrayListToArray.java
+++ DSA/src/main/java/arrayList/ArrayListToArray.java
@@ -8,6 +8,7 @@ public class ArrayListToArray {
 		list.add(2);
 		list.add(3);
 		list.add(4);
+		list.e
 
 		int[] arr=new int[3];
 		for(int i=0;i<arr.length;i++) {
